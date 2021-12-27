a3732ca10d1dce66d5d92a1db859d2b9af73ea34
Kovalenko Vasyl <kovalenkova26@gmail.com>
2021-12-27 21:47:36 +0200

    add adaptation

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,3 +1,4 @@
+
 new Swiper('.swiper', {
         slidesPerView: 1,
         spaceBetween: 30,
@@ -5,8 +6,25 @@ new Swiper('.swiper', {
         navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
-        }
+        },
+        pagination: {
+          el: '.swiper-pagination',
+          type: 'bullets',
+        },
 });
 
 
 new WOW().init();
+
+
+const burger = document.querySelector('.header__burger');
+const menuBurger = document.querySelector('.menu');
+
+burger.addEventListener('click', () => {
+  menuBurger.classList.toggle('active');
+  burger.classList.toggle('active');
+});
+
+// closeElem.addEventListener('click', () => {
+//   menu.classList.remove('active');
+// });
