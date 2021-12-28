5911d308707f5620b36ba9624111141cb70fe403
Kovalenko Vasyl <kovalenkova26@gmail.com>
2021-12-29 00:31:28 +0200

    Updates

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+
 new Swiper('.swiper', {
         slidesPerView: 1,
         spaceBetween: 30,
@@ -5,8 +6,32 @@ new Swiper('.swiper', {
         navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
-        }
+        },
+        pagination: {
+          el: '.swiper-pagination',
+          type: 'bullets',
+        },
 });
 
 
 new WOW().init();
+
+//burger menu
+const burger = document.querySelector('.header__burger');
+const menuBurger = document.querySelector('.menu');
+
+burger.addEventListener('click', () => {
+  menuBurger.classList.toggle('active');
+  burger.classList.toggle('active');
+});
+
+
+// footer menu 
+const footerButton = document.querySelectorAll('.footer__header');
+const footerMenu = document.querySelectorAll('.footer__ul');
+console.log(footerButton);
+
+footerButton.addEventListener('click', () => {
+  footerMenu.classList.toggle('active');
+  footerButton.classList.toggle('active');
+});
