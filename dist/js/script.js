db7f88689970323601911b240e4f6661b169c3fc
Thimovez <thimovez@gmail.com>
2022-01-10 03:14:19 +0300

    adaptation

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,12 +2,12 @@ new Swiper('.swiper', {
   slidesPerView: 1,
   spaceBetween: 30,
   loop: true,
-  //Arrows
+  // Arrows
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
-  //Pagination
+  // Pagination
   pagination: {
     el: '.swiper-pagination',
     type: 'fraction',
@@ -19,15 +19,15 @@ new Swiper('.swiper', {
       return ('0' + number).slice(-2);
     },
     renderFraction: function (currentClass, totalClass) {
-      return '<span class="'+ currentClass + '"></span>' + 
-      '<div class="oblique-divider"></div>' + 
-      '<span class="' + totalClass + '"></span>';
+      return '<span class="' + currentClass + '"></span>' +
+        '<div class="oblique-divider"></div>' +
+        '<span class="' + totalClass + '"></span>';
     },
   },
 });
 
 
-//burger menu for phone
+// Burger menu for phone
 const burger = document.querySelector('.header__burger');
 const menuBurger = document.querySelector('.menu');
 
@@ -37,17 +37,48 @@ burger.addEventListener('click', () => {
 });
 
 
-// footer menu  phone
-let footerButton = document.querySelectorAll('.footer__header');
-let footerMenu = document.querySelectorAll('.footer__ul');
+// Footer menu phone
+// let footerButton = document.querySelectorAll('.footer__header');
+// let footerMenu = document.querySelector('.footer__ul');
 
-function show(){
-  footerButton.forEach(el => 
-    el.addEventListener('click', function() {
-      footerButton.forEach(item => item.classList.toggle('active'));
-      footerMenu.forEach(item => item.classList.toggle('active'));
-    })
-  );
-}
+// footerButton.forEach(accordionItem => {
+//   accordionItem.footerMenu.addEventListener('click', (e) => {
+//     accordionItem.classList.toggle('active');
+//   });
 
-show();
+// });
+
+
+
+// function show() {
+//   footerButton.forEach(el =>
+//     el.addEventListener('click', function () {
+//       footerButton.forEach(item => item.classList.toggle('active'));
+//       footerMenu.forEach(item => item.classList.toggle('active'));
+//     })
+//   );
+// }
+
+// show();
+
+
+
+// const accordion_items_elms = document.querySelectorAll('.accordion .accordion__item');
+
+// accordion_items_elms.forEach(accordionItem => {
+
+//   accordionItem.querySelector('.accordion__item__head').addEventListener('click', (e) => {
+//     accordionItem.classList.toggle('active');
+//   });
+
+// });
+
+const footerUl = document.querySelectorAll('.footer__ul');
+
+footerUl.forEach(accordionItem => {
+
+  accordionItem.querySelector('.footer__header').addEventListener('click', (e) => {
+    accordionItem.classList.toggle('active');
+  });
+
+});
