fb5e66c106d70c47232afbfe44c2e21d89be50ca
Thimovez <thimovez@gmail.com>
2022-01-10 03:14:42 +0300

    Updates

--- js/script.js
+++ js/script.js
@@ -1,22 +1,33 @@
-
 new Swiper('.swiper', {
-        slidesPerView: 1,
-        spaceBetween: 30,
-        loop: true,
-        navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-        },
-        pagination: {
-          el: '.swiper-pagination',
-          type: 'bullets',
-        },
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  // Arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  // Pagination
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'fraction',
+    // Custom fraction
+    formatFractionCurrent: function (number) {
+      return ('0' + number).slice(-2);
+    },
+    formatFractionTotal: function (number) {
+      return ('0' + number).slice(-2);
+    },
+    renderFraction: function (currentClass, totalClass) {
+      return '<span class="' + currentClass + '"></span>' +
+        '<div class="oblique-divider"></div>' +
+        '<span class="' + totalClass + '"></span>';
+    },
+  },
 });
 
 
-new WOW().init();
-
-//burger menu
+// Burger menu for phone
 const burger = document.querySelector('.header__burger');
 const menuBurger = document.querySelector('.menu');
 
@@ -26,12 +37,48 @@ burger.addEventListener('click', () => {
 });
 
 
-// footer menu 
-const footerButton = document.querySelectorAll('.footer__header');
-const footerMenu = document.querySelectorAll('.footer__ul');
-console.log(footerButton);
+// Footer menu phone
+// let footerButton = document.querySelectorAll('.footer__header');
+// let footerMenu = document.querySelector('.footer__ul');
 
-footerButton.addEventListener('click', () => {
-  footerMenu.classList.toggle('active');
-  footerButton.classList.toggle('active');
+// footerButton.forEach(accordionItem => {
+//   accordionItem.footerMenu.addEventListener('click', (e) => {
+//     accordionItem.classList.toggle('active');
+//   });
+
+// });
+
+
+
+// function show() {
+//   footerButton.forEach(el =>
+//     el.addEventListener('click', function () {
+//       footerButton.forEach(item => item.classList.toggle('active'));
+//       footerMenu.forEach(item => item.classList.toggle('active'));
+//     })
+//   );
+// }
+
+// show();
+
+
+
+// const accordion_items_elms = document.querySelectorAll('.accordion .accordion__item');
+
+// accordion_items_elms.forEach(accordionItem => {
+
+//   accordionItem.querySelector('.accordion__item__head').addEventListener('click', (e) => {
+//     accordionItem.classList.toggle('active');
+//   });
+
+// });
+
+const footerUl = document.querySelectorAll('.footer__ul');
+
+footerUl.forEach(accordionItem => {
+
+  accordionItem.querySelector('.footer__header').addEventListener('click', (e) => {
+    accordionItem.classList.toggle('active');
+  });
+
 });
